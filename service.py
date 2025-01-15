63e4382d779291ee1f81db7ec57245ff8c9e4d08
Santiago Ruiz <hiebra@hotmail.com>
2025-01-15 16:02:03 +0100

    x

--- service.py
+++ service.py
@@ -1,8 +1,8 @@
 def run():
     import xbmc
     import xbmcgui
+    # Run the OneDrive addon and navigate to the images section
     url = 'plugin://plugin.onedrive/?content_type=image'
-    xbmc.executebuiltin(f'ActivateWindow(Videos,{url})')
-
+    xbmc.executebuiltin(f'Container.Update({url})')
 if __name__ == "__main__":
     run()
