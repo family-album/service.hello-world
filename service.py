e6a1c75cd6b19dcaf9031964d26c65a9b748c18a
Santiago Ruiz <hiebra@hotmail.com>
2025-01-15 20:11:36 +0100

    Sin comentarios

--- service.py
+++ service.py
@@ -1,7 +1,7 @@
 def run():
     import xbmc
     #url = 'plugin://plugin.onedrive/?content_type=image'
-    url = 'http://localhost:8586/source/OneDrive/?content_type=image'
+    url = 'http://localhost:8586/source/OneDrive/'
     xbmc.executebuiltin('ActivateWindow(Pictures)')
     xbmc.executebuiltin(f'Container.Update({url})')
 
