ef3ce7f41ff588fd8e4516e871a3c9902243ca19
Santiago Ruiz <hiebra@hotmail.com>
2025-01-15 14:04:05 +0100

    Se añade salto a las imágines de plubin.onedrive en arranque

--- service.py
+++ service.py
@@ -6,5 +6,8 @@ def run():
     dialog = xbmcgui.Dialog()
     dialog.ok("Hello World", "Hello World")
 
+    # Ejecutar el addon de imágenes en la sección de imágenes
+    xbmc.executebuiltin('RunAddon(plugin.onedrive, content=images)')
+
 if __name__ == "__main__":
     run()
