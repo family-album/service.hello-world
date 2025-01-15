2ef4227a5e1f7147a55d476a931f8749fb16a9f0
Santiago Ruiz <hiebra@hotmail.com>
2025-01-15 14:47:55 +0100

    x

--- service.py
+++ service.py
@@ -1,7 +1,4 @@
 def run():
-    import xbmc
-    addon_id = 'plugin.onedrive'
-    url = f'plugin://{addon_id}/'
-    xbmc.executebuiltin(f'RunPlugin({url}?content_type=images)')
+    print("Service is running...")
 if __name__ == "__main__":
     run()
