cb63e776b8bf7267f2465260e3fd5d361487451b
Santiago Ruiz <hiebra@hotmail.com>
2025-01-15 16:11:02 +0100

    x

--- service.py
+++ service.py
@@ -1,15 +1,6 @@
 def run():
     import xbmc
-    import xbmcgui
-
-    # Create a dialog window with "Hello, World!" message
-    dialog = xbmcgui.Dialog()
-    dialog.ok("Hello, World!", "Hello, World!")
-
-    # Run the OneDrive addon and navigate to the images section
-    url = 'plugin://plugin.onedrive/?content_type=image'
     xbmc.executebuiltin('ActivateWindow(Pictures)')
     xbmc.executebuiltin(f'Container.Update({url})')
-
 if __name__ == "__main__":
     run()
