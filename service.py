fb252cc95fb69f95600f31e1c5ccfafd81be221e
Santiago Ruiz <hiebra@hotmail.com>
2025-01-15 19:04:35 +0100

    z

--- service.py
+++ service.py
@@ -1,6 +1,8 @@
 def run():
     import xbmc
+    url = 'plugin://plugin.onedrive/?content_type=image'
     xbmc.executebuiltin('ActivateWindow(Pictures)')
     xbmc.executebuiltin(f'Container.Update({url})')
+
 if __name__ == "__main__":
     run()
